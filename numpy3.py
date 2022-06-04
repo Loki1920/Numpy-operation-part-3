8f8ef338e171cfaf254e5ffe5201610bb6f85953
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-06-04 11:47:01 +0530

    Add files via upload

---
+++ numpy3.py
@@ -0,0 +1,15 @@
+import numpy as np 
+matrix1 = np.array([ [1,2,3],
+                   [4,14,15],
+                   [6,7,8]])
+matrix2 = np.array([ [1,2,3],
+                     [4,14,15],
+                     [6,7,8]])
+
+#addition of matrix
+print("addition:",matrix1 + matrix2)
+#multiplication
+print("product :",matrix1 * matrix2)
+#sqrt
+print("square root of matrix1:",np.sqrt(matrix1))
+
